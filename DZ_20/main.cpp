532cfa422e089cd2ee55dcec03c4cad057709989
stragesnight <alex.spresyo.o@gmail.com>
2021-07-23 20:45:55 +0300

    DZ_20 macro system && conspect update

--- DZ_20/main.cpp
+++ DZ_20/main.cpp
@@ -1,9 +1,9 @@
 /*
-	Домашнее задание №20
-		Ученик: Шелест Александр
+	�������� ������� �20
+		������: ������ ���������
 
-	Написать пример программы с использованием 
-	абстрактного класса и виртуального деструктора.
+	�������� ������ ��������� � ��������������
+	������������ ������ � ������������ �����������.
 */
 
 #include <iostream>
@@ -21,7 +21,7 @@
 class Node;
 
 std::map<std::string, Node*> expressions;
-std::map<std::string, const char*> macro_expressions;
+std::map<std::string, void(*)(const char*)> directives;
 
 class Node
 {
@@ -33,7 +33,7 @@ public:
 	Node(const char* name, size_t nArgs = 0)
 	{
 		this->nArgs = nArgs;
-	
+
 		size_t len = strlen(name);
 		this->name = new char[len + 1];
 
@@ -49,7 +49,7 @@ public:
 
 	size_t getNArgs() { return nArgs; }
 	const char* getName() { return name; }
-	
+
 	virtual long evaluate(std::vector<Node*>* args) = 0;
 };
 
@@ -82,7 +82,7 @@ class ExpressionNode : public Node
 {
 private:
 	long (*func)(std::vector<Node*>*);
-	
+
 public:
 	ExpressionNode(const char* name, size_t nArgs, long(*func)(std::vector<Node*>*))
 		: Node(name, nArgs)
@@ -101,6 +101,63 @@ public:
 	}
 };
 
+/////////////////////////////////////////////////////////////////////////////////////
+
+long parseNumber(const char* buff)
+{
+	long result = 0;
+
+	for (size_t i = strlen(buff); i > 0; i--)
+	{
+		if (buff[i - 1] == '-')
+			return -result;
+
+		result *= 10;
+		result += buff[i - 1] - '0';
+	}
+
+	return result;
+}
+
+void skipWS(const char* str, size_t& i)
+{
+	while (str[i] == ' ' || str[i] == '\t')
+		i++;
+}
+
+int readWord(const char* src, char* buff, size_t& i)
+{
+	size_t len = 0;
+
+	skipWS(src, i);
+
+	if (src[i] == '!')
+	{
+		buff[len] = src[i];
+		len++;
+		i++;
+		return len;
+	}
+
+	while ((src[i] >= 'a' && src[i] <= 'z')
+		|| (src[i] >= 'A' && src[i] <= 'Z')
+		|| (src[i] >= '0' && src[i] <= '9')
+		|| (src[i] == '-'))
+	{
+		buff[len] = src[i];
+		len++;
+		i++;
+	}
+
+	return len;
+}
+
+/////////////////////////////////////////////////////////////////////////////////////
+
+void def(const char* str);
+
+/////////////////////////////////////////////////////////////////////////////////////
+
 class MacroNode : public Node
 {
 private:
@@ -108,70 +165,33 @@ private:
 	{
 		SYMBOL,
 		NUMERIC,
+		DIRECTIVE,
 		UNRECOGNIZED
 	};
 
 	char* expr;
-	
-	void skipWS(const char* str, size_t& i)
-	{
-		while (str[i] == ' ' || str[i] == '\t')
-			i++;
-	}
 
 	WORD_TYPE getWordType(const char* str)
 	{
+		if (*str == '!')
+			return WORD_TYPE::DIRECTIVE;
+
 		if ((*str >= 'a' && *str <= 'z')
 			|| (*str >= 'A' && *str <= 'Z'))
 			return WORD_TYPE::SYMBOL;
-		
+
 		if ((*str >= '0' && *str <= '9')
 			|| (*str == '-'))
 			return WORD_TYPE::NUMERIC;
-	
+
 		return WORD_TYPE::UNRECOGNIZED;
-	}
-
-	long parseNumber(const char* buff)
-	{
-		long result = 0;
-
-		for (size_t i = strlen(buff); i > 0; i--)
-		{
-			if (buff[i - 1] == '-')
-				return -result;
-
-			result *= 10;
-			result += buff[i - 1] - '0';
-		}
-
-		return result;
-	}
-	
-	int readWord(const char* src, char* buff, size_t& i)
-	{
-		size_t len = 0;
-
-		skipWS(src, i);
-
-		while((src[i] >= 'a' && src[i] <= 'z')
-			|| (src[i] >= 'A' && src[i] <= 'Z')
-			|| (src[i] >= '0' && src[i] <= '9')
-			|| (src[i] == '-'))
-		{
-			buff[len] = src[i];
-			len++;
-			i++;
-		}
-
-		return len;
 	}
 
 	long evalNode(Node* node, const char* rest, size_t& i)
 	{
 		size_t nArgs = node->getNArgs();
 
-		char buff[64] {};
+		char buff[64]{};
 		std::vector<Node*> args;
 
 		for (size_t n = 0; n < nArgs; n++)
@@ -209,13 +229,13 @@ private:
 				logerr("unexpected symbol \"" << buff << '\"');
 				return -1;
 			}
-		
+
 			memset(buff, 0, 64);
 		}
-		
+
 		return node->evaluate(&args);
 	}
-	
+
 public:
 	MacroNode(const char* name, const char* expr)
 		: Node(name, 0)
@@ -236,8 +256,9 @@ public:
 	long evaluate(std::vector<Node*>* args) override
 	{
 		long result = -1;
-		
-		//expand();
+
+		if (strlen(expr) == 0)
+			return 0;
 
 		size_t i = 0;
 		char buff[64]{};
@@ -265,6 +286,23 @@ public:
 			result = evalNode(node, expr, i);
 			break;
 		}
+		case WORD_TYPE::DIRECTIVE:
+		{
+			memset(buff, 0, 64);
+			readWord(expr, buff, i);
+
+			void(*directive)(const char*) = directives[buff];
+
+			if (directive == nullptr)
+			{
+				logerr("directive \"" << buff << "\" is undefined");
+				return -1;
+			}
+
+			directive(expr + i);
+
+			return 1;
+		}
 		case WORD_TYPE::UNRECOGNIZED:
 		default:
 		{
@@ -272,18 +310,32 @@ public:
 			return -1;
 		}
 		}
-		
+
 		memset(buff, 0, 64);
 		if (readWord(expr, buff, i))
 		{
 			logerr("unexpected symbol \"" << buff << "\" after finished expression");
 			logerr("unexpected tail symbol would be ignored");
 		}
-			
+
 		return result;
 	}
 };
 
+/////////////////////////////////////////////////////////////////////////////////////
+
+void def(const char* str)
+{
+	char buff[64] {};
+	size_t i = 0;
+
+	readWord(str, buff, i);
+	MacroNode* to_add = new MacroNode(buff, str + i);
+
+	expressions[buff] = to_add;
+}
+
+/////////////////////////////////////////////////////////////////////////////////////
 
 long add(std::vector<Node*>* args)
 {
@@ -315,6 +367,8 @@ long odd(std::vector<Node*>* args)
 	return (args->at(0)->evaluate(nullptr) & 1) == 1;
 }
 
+/////////////////////////////////////////////////////////////////////////////////////
+
 int main()
 {
 	setlocale(LC_ALL, "rus");
@@ -325,8 +379,8 @@ int main()
 	expressions["div"] = new ExpressionNode("div", 2, div);
 	expressions["even"] = new ExpressionNode("even", 1, even);
 	expressions["odd"] = new ExpressionNode("odd", 1, odd);
-	expressions["a"] = new NumericNode("a", 2);
-	expressions["b"] = new NumericNode("b", 3);
+
+	directives["def"] = def;
 
 	while (true)
 	{
@@ -336,9 +390,9 @@ int main()
 
 		MacroNode expression("expr", input);
 		long result = expression.evaluate(nullptr);
-		log("evaluated to " << result);
+		log("result: " << result);
 	}
-	
+
 	std::cin.get();
 	expressions.clear();
 	return 0;
