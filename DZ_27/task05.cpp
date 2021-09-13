b1f8709f0888804419476cbfdaf29cb6da880078
ishiki <ishiki@system.lan>
2021-09-13 14:10:42 +0300

    stuff

---
+++ DZ_27/task05.cpp
@@ -0,0 +1,345 @@
+﻿/* 
+ * 	-= Домашнее Задание №27, Задача № 5 =-
+ * 		Ученик: Шелест Александр
+ *
+ * 	Создать динамический массив, хранящий в себе последовательность
+ * 	из имён и номеров телефонов. 
+ * 	Реализовать возможность поиска, добавления и изменения данных.
+ */
+
+#include <iostream>
+#include <string.h>
+#include <clocale>
+#include <ctime>
+
+#if defined (_WIN32) or defined (_WIN64)
+# include <Windows.h>
+#endif
+
+static const char* names[] = { 	
+	"имя1", "имя2", "имя3", "имя4", "имя5",
+	"имя6", "имя7", "имя8", "имя9", "имя10"
+};
+static const unsigned n_names = 10;
+
+static const char* phones[] = { 
+	"телефон1", "телефон2", "телефон3", "телефон4", "телефон5",
+	"телефон6", "телефон7", "телефон8", "телефон9", "телефон10" 
+};
+static const unsigned n_phones = 10;
+
+
+// безопасный ввод значения с проверкой на корректность
+template <class T>
+void safe_input(T& v, const char* msg, bool(*predicate)(const T&) = nullptr)
+{
+	while (true)
+	{
+		std::cout << msg;
+		std::cin >> v;
+
+		bool success = std::cin.good();
+
+		if (predicate)
+			success = success || predicate(v);
+
+		std::cin.clear();
+		std::cin.ignore(16, '\n');
+
+		if (success)
+			return;
+
+		std::cout << "Введено некорректное значение\n";
+	}
+}
+
+// безопасный ввод строки с проверкой на корректность
+char* safe_getline(const char* msg)
+{
+	char* buff = new char[256]{};
+
+	while (true)
+	{
+		std::cout << msg;
+		std::cin.getline(buff, 256);
+
+		if (std::cin.good())
+			return buff;
+
+		std::cout << "Введено некорректное значение\n";
+		memset(buff, 0, 256);
+	}
+
+	return nullptr;
+}
+
+// контакт телефонной книги
+struct entry
+{
+	char* name = nullptr;
+	char* phone = nullptr;
+
+	entry() {}
+	// конструктор структуры, копирует параметры себе в поля
+	entry(const char* const name, const char* const phone)
+	{
+		unsigned namelen = strlen(name) + 1;
+		unsigned phonelen = strlen(phone) + 1;
+
+		this->name = new char[namelen]{};
+		this->phone = new char[phonelen]{};
+
+		memcpy(this->name, name, namelen);
+		memcpy(this->phone, phone, phonelen);
+	}
+
+	// освободить динамическую память объекта
+	void free()
+	{
+		if (name != nullptr)
+			delete[] name;
+		if (phone != nullptr)
+			delete[] phone;
+	}
+
+	// присвоить объекту новое значение
+	entry& operator=(const entry& other)
+	{
+		this->free();
+
+		unsigned namelen = strlen(other.name) + 1;
+		unsigned phonelen = strlen(other.phone) + 1;
+
+		this->name = new char[namelen]{};
+		this->phone = new char[phonelen]{};
+
+		memcpy(this->name, other.name, namelen);
+		memcpy(this->phone, other.phone, phonelen);
+
+		return *this;
+	}
+
+	// сравнить два объекта
+	bool operator==(const entry& other)
+	{
+		return (strcmp(name, other.name) == 0) 
+			|| (strcmp(phone, other.phone) == 0);
+	}
+
+	// сравнить контакт со строкой
+	bool operator==(const char* const str)
+	{
+		return (strcmp(name, str) == 0) 
+			|| (strcmp(phone, str) == 0);
+	}
+};
+
+// телефонная книга
+struct phonebook
+{
+	entry* entries = nullptr;
+	size_t size = 0;
+
+	// освободить динамическую память объекта
+	void free()
+	{
+		if (entries == nullptr)
+			return;
+
+		for (size_t i = 0; i < size; i++)
+			entries[i].free();
+	}
+
+	// оператор индексации контактов
+	entry& operator[](size_t index) const
+	{
+		index = (index >= size) ? size - 1 : index;
+		return entries[index];
+	}
+
+	// добавить новый контакт в книгу
+	phonebook& operator+=(const entry& e)
+	{
+		entry* res = new entry[size + 1]{};
+
+		for (size_t i = 0; i < size; i++)
+			res[i] = entries[i];
+
+		res[size] = e;
+
+		size++;
+		if (entries != nullptr)
+			delete[] entries;
+		entries = res;
+
+		return *this;
+	}
+
+	// найти контакты в книге, которые подходят по критерию
+	phonebook operator&(const entry& e)
+	{
+		phonebook res;
+
+		for (unsigned i = 0; i < size; i++)
+		{
+			if (entries[i] == e)
+				res += entries[i];
+		}
+
+		return res;
+	}
+
+	// найти контакты в книге, которые подходят по критерию
+	phonebook operator&(const char* const str)
+	{
+		phonebook res;
+
+		for (unsigned i = 0; i < size; i++)
+		{
+			if (entries[i] == str)
+				res += entries[i];
+		}
+
+		return res;
+	}
+};
+
+
+// вывод контакта на экран
+std::ostream& operator<<(std::ostream& ostr, const entry& e)
+{
+	ostr << e.name << ", " << e.phone;
+	return ostr;
+}
+
+// вывод телефонной книги на экран
+std::ostream& operator<<(std::ostream& ostr, const phonebook& pb)
+{
+	if (pb.size == 0)
+	{
+		ostr << "пусто!\n";
+		return ostr;
+	}
+
+	for (size_t i = 0; i < pb.size; i++)
+		ostr << i + 1 << ". " << pb[i] << '\n';
+	return ostr;
+}
+
+
+// заполнить книгу случайными значениями
+void phonebook_fill(phonebook& pb, size_t amount)
+{
+	for (size_t i = 0; i < amount; i++)
+	{
+		const char* name = names[rand() % n_names];
+		const char* phone = phones[rand() % n_phones];
+		pb += entry(name, phone);
+	}
+}
+
+// добавить новый контакт в книгу
+void phonebook_addentry(phonebook& pb)
+{
+	char* name = safe_getline("имя нового контакта: ");
+	char* phone = safe_getline("номер телефона нового контакта: ");
+
+	pb += entry(name, phone);
+
+	std::cout << "\nконтакт успешно добавлен в книгу\n";
+}
+
+// искать контакты в книге по заданному критерию
+void phonebook_searchentry(phonebook& pb)
+{
+	char* query = safe_getline("введите критерий для поиска (имя или телефон контакта): ");
+
+	std::cout << "результаты поиска:\n";
+	std::cout << (pb & query) << '\n';
+}
+
+// изменить контакт в книге
+void phonebook_editentry(phonebook& pb)
+{
+	size_t index = 0;
+	safe_input(index, "введите номер контакта, который нужно изменить: ");
+
+	char* name = safe_getline("новое имя контакта: ");
+	char* phone = safe_getline("новый номер телефона контакта: ");
+
+	pb[index] = entry(name, phone);
+
+	std::cout << "\nданные контакта успешно изменены\n";
+}
+
+
+int main()
+{
+	srand(time(0));
+	setlocale(LC_ALL, "rus");
+#if defined (_WIN32) or defined (_WIN64)
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+#endif
+
+	std::cout << "\t-= Домашнее Задание №27, Задача № 5 =-\n"
+ 		<< "\t\tУченик: Шелест Александр\n\n"
+ 		<< "Создать динамический массив, хранящий в себе последовательность\n"
+ 		<< "из имён и номеров телефонов.\n"
+ 		<< "Реализовать возможность поиска, добавления и изменения данных.\n\n";
+
+	std::cout << "нажмите Enter для выхода в меню...\n";
+	std::cin.get();
+
+	phonebook pb;
+	phonebook_fill(pb, 8);
+
+	int input = -1;
+	while (input != 0)
+	{
+		std::cout << "\t+============+\n";
+		std::cout << "\t||   МЕНЮ   ||\n";
+		std::cout << "\t+============+\n\n";
+		std::cout << "1. вывести телефонную книгу на экран\n";
+		std::cout << "2. добавить новый контакт\n";
+		std::cout << "3. искать контакт в книге\n";
+		std::cout << "4. редактировать контакт в книге\n";
+		std::cout << "\n0. выход из программы\n\n";
+
+		safe_input(input, "выберите действие по его номеру в меню: ");
+
+		switch (input)
+		{
+		case 1:
+			std::cout << "*ТЕЛЕФОННАЯ КНИГА*\n\n";
+			std::cout << pb << '\n';
+			break;
+		case 2:
+			std::cout << "*ДОБАВЛЕНИЕ НОВОГО КОНТАКТА*\n\n";
+			phonebook_addentry(pb);
+			break;
+		case 3:
+			std::cout << "*ПОИСК КОНТАКТА*\n\n";
+			phonebook_searchentry(pb);
+			break;
+		case 4:
+			std::cout << "*РЕДАКТИРОВАНИЕ КОНТАКТА*\n\n";
+			phonebook_editentry(pb);
+			break;
+		case 0:
+			exit(0);
+		default:
+			std::cout << "\nвведен неверный пункт меню\n";
+			break;
+		}
+
+		std::cout << "\nнажмите Enter для продолжения...\n";
+		std::cin.get();
+	}
+ 
+	pb.free();
+
+	std::cin.get();
+	return 0;
+}
+
---
+++ DZ_28/main.cpp
@@ -0,0 +1,274 @@
+﻿/*
+ * 	-= Домашнее Задание №28 =-
+ * 		Ученик: Шелест Александр
+ *
+ * 	Создать пользовательские классы для умных указателей,
+ * 	повторив поведение unique_ptr и shared_ptr.
+ */
+
+// закомментируйте эту сттроку для использования умных указателей 
+// из стандартной библиотеки
+#define MY_SMARTPR_IMPL
+
+#include <iostream>
+#include <clocale>
+
+#ifndef MY_SMARTPR_IMPL
+# 	include <memory>
+	using std::shared_ptr;
+	using std::unique_ptr;
+#else
+
+// класс, содержащий общие поля и методы для все классов умных указателей
+template <class T>
+class smartptr_common
+{
+protected:
+	T* ptr; 	// указатель на непосредственно данные
+
+public:
+	// логические операторы
+	explicit operator bool() const { return ptr != nullptr; }
+	bool operator==(const void* ptr) const { return this->ptr == ptr; }
+	bool operator==(std::nullptr_t) const { return this->ptr == nullptr; }
+	bool operator!=(const void* ptr) const { return this->ptr != ptr; }
+	bool operator!=(std::nullptr_t) const { return this->ptr != nullptr; }
+	// запретить присваивать объектам значения через опертор =
+	template <class arg> void operator=(const arg& other) = delete;
+	// опертор разыменования
+	T& operator*() const { return *ptr; }
+
+	// удалить старый указатель и заменить его новым
+	void reset(T* ptr)
+	{
+		T* old_ptr = this->ptr;
+		this->ptr = ptr;
+
+		if (old_ptr != nullptr)
+			delete old_ptr;
+	}
+
+	// освободить внутреннее поле и возвратить его содержание
+	T* release()
+	{
+		T* tmp = ptr;
+		this->ptr = nullptr;
+
+		return tmp;
+	}
+
+	// получить внутренний указатель
+	T* get() const { return ptr; }
+};
+
+// класс умного указателя, для которого может быть только 
+// один уникальный объект-владелец, все попытки копирования отсекаются.
+template <class T>
+class unique_ptr : public smartptr_common<T>
+{
+public:
+	// конструктор с параметром
+	explicit unique_ptr(T* ptr = nullptr)
+	{
+		this->ptr = ptr;
+	}
+	// запретить копирование через конструктор
+	unique_ptr(const unique_ptr& other) = delete;
+
+	// деструктор попросту освобождает указатель
+	~unique_ptr()
+	{
+		delete this->ptr;
+	}
+
+	// поменять указатели местами
+	void swap(unique_ptr& other)
+	{
+		T* tmp = this->ptr;
+		this->ptr = other.ptr;
+		other.ptr = tmp;
+	}
+};
+
+// класс умного указателя, для которого может быть какое угодно число владельцев.
+// при этом хранится количество его владельцев для контроля ситуации.
+template <class T>
+class shared_ptr : public smartptr_common<T>
+{
+private:
+	size_t* refcount; 	// количество владельцев указателя
+
+public:
+	// конструктор
+	shared_ptr(T* ptr = nullptr)
+	{
+		this->ptr = ptr;
+		refcount = new size_t(ptr != nullptr);
+	}
+
+	// если параметром выступает другой такой же указатель, 
+	// количество владельцев инкрементируется
+	shared_ptr(shared_ptr& other)
+	{
+		(*other.refcount)++;
+		this->refcount = other.refcount;
+		this->ptr = other.ptr;
+	}
+
+	// деструктор
+	// проверяет количество владельцев указателя
+	// освобождает память только если остался только один владелец
+	~shared_ptr()
+	{
+		if (*refcount == 1)
+		{
+			delete refcount;
+			delete this->ptr;
+			return;
+		}
+
+		(*refcount)--;
+	}
+
+	// поменять указатели местами
+	void swap(shared_ptr& other)
+	{
+		T* tmp = this->ptr;
+		this->ptr = other.ptr;
+		other.ptr = tmp;
+	}
+
+	// возвращает истину, если у указателя только один владелец
+	bool unique() const { return *refcount == 1; }
+	// возвращает количество владельцев указателя
+	size_t use_count() const { return *refcount; }
+};
+#endif
+
+
+// вывод информации об уникальном указателе на экран
+template <class T>
+void printptr(const unique_ptr<T>& up)
+{
+	if (up == nullptr)
+		std::cout << "нулевой указатель!\n\n";
+	else
+	{
+		std::cout << "значение указателя на \"" 
+			<< up.get() << "\" равно \"" << *up << "\"\n";
+	}
+}
+
+// вывод информации об общем указателе на экран
+template <class T>
+void printptr(const shared_ptr<T>& sp)
+{
+	if (sp == nullptr)
+		std::cout << "нулевой указатель!\n\n";
+	else
+	{
+		std::cout << "значение указателя на \"" 
+			<< sp.get() << "\" равно \"" << *sp << "\"\n";
+		std::cout << "количество владельцев для данного указателя: " 
+			<< sp.use_count() << '\n';
+		std::cout << "уникален ли указатель? - ";
+		if (sp.unique())
+			std::cout << "да\n";
+		else
+			std::cout << "нет\n";
+	}
+}
+
+
+int main()
+{
+# 	define pause_exec() \
+	std::cout << "\nнажмите Enter для продолжения..."; \
+	std::cin.get();
+
+	setlocale(LC_ALL, "rus");
+
+	std::cout << "\t-= Домашнее Задание №28 =-\n"
+ 		<< "\t\tУченик: Шелест Александр\n\n"
+ 		<< "Создать пользовательские классы для умных указателей,\n"
+ 		<< "повторив поведение unique_ptr и shared_ptr.\n\n";
+
+
+	// unique_ptr
+	
+	std::cout << "=== UNIQUE_PTR ===\n\n";
+	std::cout << "создание нового объекта unique_ptr с именем up1:\n";
+	unique_ptr<int> up1(new int(8));
+	printptr(up1);
+	pause_exec();
+	
+	std::cout << "\nсоздание нового объекта unique_ptr с именем up2:\n";
+	// unique_ptr<int> up2(up1);  <- ошибка!
+	unique_ptr<int> up2(new int(9));
+	printptr(up2);
+	pause_exec();
+
+	std::cout << "\nуказатели меняются местами...\n";
+	// up2 = up1;  <- ошибка!
+	up1.swap(up2);
+	std::cout << "up1: ";
+	printptr(up1);
+	std::cout << "up2: ";
+	printptr(up2);
+	pause_exec();
+
+	std::cout << "\nосвобождение указателя up1...\n";
+	up1.reset(nullptr);
+	std::cout << "up1: ";
+	printptr(up1);
+	pause_exec();
+
+	// shared_ptr
+	
+	std::cout << "\n=== SHARED_PTR ===\n\n";
+	std::cout << "создание нового объекта shared_ptr с именем sp1:\n";
+	shared_ptr<int> sp1(new int(16));
+	printptr(sp1);
+	pause_exec();
+
+	{
+		std::cout << "\nсоздание нового объекта shared_ptr с именем sp2,\n"
+			<< "\tкоторый наследует указатель sp1:\n";
+		shared_ptr<int> sp2(sp1);
+
+		printptr(sp2);
+		pause_exec();
+
+		std::cout << "\nизменение значения указателя sp1...\n\n";
+		*sp1 = 5;
+
+		std::cout << "sp1: ";
+		printptr(sp1);
+		pause_exec();
+
+		{
+			std::cout << "\nсоздание нового объекта shared_ptr с именем sp3,\n"
+				<< "\tкоторый наследует указатель sp2:\n";
+			shared_ptr<int> sp3 = sp2;
+			printptr(sp3);
+			pause_exec();
+
+			std::cout << "\nудаление объекта sp3...\n";
+		}
+
+		std::cout << "удаление объекта sp2...\n\n";
+	}
+
+	std::cout << "sp1: ";
+	printptr(sp1);
+	pause_exec();
+
+	std::cout << "\nосвобождение указателя sp1...\n";
+	sp1.reset(nullptr);
+	printptr(sp1);
+
+	std::cout << "\nТест завершён. Нажмите Enter для выхода...";
+	std::cin.get();
+	return 0;
+}
+
